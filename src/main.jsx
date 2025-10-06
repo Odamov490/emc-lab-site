12bab18b12dc35effa4746f5949e8ff12369ca55
Odamov490 <piskentems@gmail.com>
2025-10-06 17:48:47 +0500

    Yangilandi

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-// main.jsx (yoki index.jsx)
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </React.StrictMode>
 );
+
+
