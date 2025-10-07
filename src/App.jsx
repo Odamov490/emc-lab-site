54efcb62e53e77e731ee896f848a2206b54003c0
Odamov490 <piskentems@gmail.com>
2025-10-07 15:22:30 +0500

    Yangilandi

--- src/App.jsx
+++ src/App.jsx
@@ -1129,7 +1129,7 @@ const navigate = useNavigate();
       return (
         <Card key={c.key} className="p-0 overflow-hidden flex flex-col h-full">
           {/* Rasm */}
-          <div className="h-full w-full overflow-hidden">
+          <div className="h-40 w-full overflow-hidden">
             <img
               src={c.image}
               alt={title}
