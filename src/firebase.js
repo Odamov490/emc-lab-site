1b3db98471045a76f30886523729833cf3c42ff0
Odamov490 <piskentems@gmail.com>
2025-11-12 16:27:08 +0500

    Yangilandi

--- src/firebase.js
+++ src/firebase.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 // Firestore init
-//export const db = getFirestore(app);
+export const db = getFirestore(app);
 export const storage = getStorage(app); 
 export const auth = getAuth(app);
-export const db = {};
+
